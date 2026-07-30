0986a39cd2f6499b4728e4bc40388bd5a9f0f8d7
VendelaAlilehto <vendelaalilehto@gmail.com>
2026-07-30 17:13:55 +0300

    add right commit

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,18 +1,14 @@
-
-def debug_print(debug_msg=None, **kwargs):
-
-    if debug_msg:
-        print(debug_msg)
-
-    for key, value in kwargs.items():
+@@ -8,6 +8,40 @@ def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
 def mergesort(array):
+    debug_print(array=array)
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
+    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -21,6 +17,8 @@ def mergesort(array):
 
 
 def merge(left, right):
+    debug_print(debug_msg="Merging...", left=left, right=right)
+
     merged = []
 
     while len(left) > 0 and len(right) > 0:
@@ -34,19 +32,18 @@ def merge(left, right):
     else:
         merged += right
 
+    debug_print(merged=merged)
     return merged
 
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-    input_list = input_str.split(",")
-    value_list = []
-    for x in input_list:
-        try:
-            value_list.append(int(x))
-        except ValueError as err:
-            print("Invalid input.")
+
+
+@@ -23,3 +57,6 @@ if __name__ == "__main__":
             quit(1)
+
+    debug_print(value_list=value_list)
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
